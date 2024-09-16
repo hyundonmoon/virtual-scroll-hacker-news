4d95dfcc6b377086d5b85a6f7ae1af37379c2a73
Hyun Don Moon <hyundonmoon@gmail.com>
2024-09-16 16:32:24 +0900

    fix import errors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import { HackerNewsVirtualList } from "./hacker-news-virtual-list.js";
+import { VirtualScrollHackerList } from "./virtual-scroll-hacker-list.js";
 
 const root = document.querySelector("#virtual-list");
 const template = document.getElementById("article__template");
 const scrollUpObserver = document.querySelector("#scroll-up-observer");
 const scrollDownObserver = document.querySelector("#scroll-down-observer");
 
-new HackerNewsVirtualList(root, template, scrollUpObserver, scrollDownObserver);
+new VirtualScrollHackerList(
+  root,
+  template,
+  scrollUpObserver,
+  scrollDownObserver
+);
